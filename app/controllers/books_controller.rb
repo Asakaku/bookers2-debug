c1db4522b41166cb7b3ddb43ffd68b77b2ec14bb
EC2 Default User <132253022+Asakaku@users.noreply.github.com>
2023-06-06 11:27:22 +0000

    task 9a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,11 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book_new = Book.new
     @post_comment = PostComment.new
-    
     @user = @book.user
+    @book_detail = Book.find(params[:id])
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book_detail.id)
+      current_user.view_counts.create(book_id: @book_detail.id)
+    end
   end
 
   def index
